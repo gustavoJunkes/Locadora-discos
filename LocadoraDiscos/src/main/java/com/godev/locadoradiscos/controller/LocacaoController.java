9029dff1154653e05e5de0e19829ad74876c3ba9
gustavoJunkes <87446204+gustavoJunkes@users.noreply.github.com>
2021-12-20 15:56:56 -0300

    Criando métodos service e controller de locação que irão

--- LocadoraDiscos/src/main/java/com/godev/locadoradiscos/controller/LocacaoController.java
+++ LocadoraDiscos/src/main/java/com/godev/locadoradiscos/controller/LocacaoController.java
@@ -1,41 +1,55 @@
 package com.godev.locadoradiscos.controller;
 
-import com.godev.locadoradiscos.dto.LocacaoDto;
-import com.godev.locadoradiscos.exception.LocacaoNotFoundException;
-import com.godev.locadoradiscos.modelo.Locacao;
-import com.godev.locadoradiscos.service.LocacaoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.godev.locadoradiscos.dto.LocacaoDto;
+import com.godev.locadoradiscos.exception.LocacaoNotFoundException;
+import com.godev.locadoradiscos.service.LocacaoService;
 
 @RestController
 @RequestMapping("/locacao")
 public class LocacaoController {
 
-    @Autowired
-    private LocacaoService locacaoService;
+	@Autowired
+	private LocacaoService locacaoService;
 
-    @PostMapping
-    public ResponseEntity<LocacaoDto> save(@RequestBody LocacaoDto locacaoDto){
-        return new ResponseEntity<>(locacaoService.save(locacaoDto), HttpStatus.OK);
-    }
+	@PostMapping
+	public ResponseEntity<LocacaoDto> save(@RequestBody LocacaoDto locacaoDto) {
+		return new ResponseEntity<>(locacaoService.save(locacaoDto), HttpStatus.OK);
+	}
 
-    @GetMapping(value = "/all")
-    public ResponseEntity<List<LocacaoDto>> getAll(){
-        return new ResponseEntity<>(locacaoService.getAll(), HttpStatus.OK);
-    }
-    
-    @DeleteMapping
-    public void delete(@Param("id") Long id) {
-    	try {
-    		locacaoService.delete(id);
-    	}
-    	catch (LocacaoNotFoundException ex) {
-    		ex.getMessage();
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<LocacaoDto>> getAll() {
+		return new ResponseEntity<>(locacaoService.getAll(), HttpStatus.OK);
+	}
+
+	@DeleteMapping
+	public void delete(@Param("id") Long id) {
+		try {
+			locacaoService.delete(id);
+		} catch (LocacaoNotFoundException ex) {
+			ex.getMessage();
 		}
-    }
+	}
+
+	@GetMapping(value = "/cliente")
+	public ResponseEntity<List<LocacaoDto>> buscarLocacoesCliente(@Param("id") Long id) {
+		return new ResponseEntity<>(locacaoService.buscarLocacoesCliente(id), HttpStatus.OK);
+	}
+
+	@GetMapping
+	public ResponseEntity<LocacaoDto> buscarPorId(@Param("id") Long id) {
+		return new ResponseEntity<>(locacaoService.buscarPorId(id), HttpStatus.OK);
+	}
 }
