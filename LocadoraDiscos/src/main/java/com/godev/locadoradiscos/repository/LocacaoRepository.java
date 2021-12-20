0ec5a364bb8621d55d875b01e070b8ffb0bc59e1
Valentina Lorencetti <valenwirth@gmail.com>
2021-12-20 15:12:45 -0300

    commit repository-query

--- LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/LocacaoRepository.java
+++ LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/LocacaoRepository.java
@@ -1,9 +1,35 @@
 package com.godev.locadoradiscos.repository;
 
 import com.godev.locadoradiscos.modelo.Locacao;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
+	
+	@Transactional
+	Optional<Locacao> findById (Long id);
+
+	@Transactional
+	Locacao findByDataLocacao (Date dataLocacao);
+
+	@Query ("SELECT max(locacao.preco) from Locacao locacao")
+	Double maiorPrecoDisco();
+
+	@Query("SELECT min(locacao.preco) from Locacao locacao")
+	Double menorPrecoDisco();
+	
+	@Query("SELECT dataDevolucao from Locacao locacao WHERE dataDevolucao > 20/12/2021 and dataDevolucao < 21/12/2021")
+	List<Locacao> buscarDevolucoes(Date dataDevolucao);
+	
+	@Query("SELECT dataLocacao from Locacao locacao WHERE dataLocacao > 20/12/2021 and dataLocacao < 21/12/2021")
+	List<Locacao> buscarLocacao(Date dataLocacao);
 }
