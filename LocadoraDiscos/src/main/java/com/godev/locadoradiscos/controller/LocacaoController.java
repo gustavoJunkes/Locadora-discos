3de228584f44872c10fa312366e8731a429752ed
JeffersonTeixeira21 <87997413+JeffersonTeixeira21@users.noreply.github.com>
2021-12-21 17:28:57 -0300

    commit do LocacaoController/Service das consultas

--- LocadoraDiscos/src/main/java/com/godev/locadoradiscos/controller/LocacaoController.java
+++ LocadoraDiscos/src/main/java/com/godev/locadoradiscos/controller/LocacaoController.java
@@ -1,5 +1,6 @@
 package com.godev.locadoradiscos.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.godev.locadoradiscos.dto.LocacaoDto;
@@ -51,5 +53,18 @@ public class LocacaoController {
 	@GetMapping
 	public ResponseEntity<LocacaoDto> buscarPorId(@Param("id") Long id) throws LocacaoNotFoundException {
 		return new ResponseEntity<>(locacaoService.buscarPorId(id), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/datalocacao")
+	public ResponseEntity<List<LocacaoDto>> buscarDataLocacao(@RequestParam("dataLocacao") Date dataLocacao){
+		List<LocacaoDto> locacoes = locacaoService.buscarDataLocacao(dataLocacao);
+		
+		return new ResponseEntity<>(locacoes, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/precomax")
+	public Double maiorPrecoDisco(@RequestParam("preco")Double preco){
+		Double locacoes = locacaoService.maiorPrecoLocacao();
+		return locacoes;
 	}
 }
