75f23bd6b5fbd34fc1ee391788822c72f246e99b
JeffersonTeixeira21 <87997413+JeffersonTeixeira21@users.noreply.github.com>
2021-12-21 10:08:26 -0300

    commit do clienteService para buscar pelo nome e cpf

--- LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/DiscoRepository.java
+++ LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/DiscoRepository.java
@@ -1,5 +1,9 @@
 package com.godev.locadoradiscos.repository;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,12 @@ import com.godev.locadoradiscos.modelo.Disco;
 @Repository
 public interface DiscoRepository extends JpaRepository<Disco, Long> {
 
+	@Transactional
+	Optional<Disco> findById (Long id);
+
+	@Transactional
+	Disco findByTitulo (String titulo);
+
+	@Transactional
+	Disco findByAutor (String autor);
 }
--- LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/LocacaoRepository.java
+++ LocadoraDiscos/src/main/java/com/godev/locadoradiscos/repository/LocacaoRepository.java
@@ -1,6 +1,12 @@
 package com.godev.locadoradiscos.repository;
 
+import java.util.Date;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.godev.locadoradiscos.modelo.Locacao;
@@ -8,4 +14,16 @@ import com.godev.locadoradiscos.repository.custom.LocacaoRepositoryCustom;
 
 @Repository
 public interface LocacaoRepository extends JpaRepository<Locacao, Long>, LocacaoRepositoryCustom {
+	
+	@Transactional
+	Optional<Locacao> findById (Long id);
+
+	@Transactional
+	Locacao findByDataLocacao (Date dataLocacao);
+
+	@Query ("SELECT max(locacao.preco) from Locacao locacao")
+	Double maiorPrecoDisco();
+
+	@Query("SELECT min(locacao.preco) from Locacao locacao")
+	Double menorPrecoDisco();
 }
